124859a2f7f13faefbbf10c191642283dcb42a9c
David Röhrscheid <david.roehrscheid@gmail.com>
2014-04-01 08:44:04 +0900

    Some more graphs. Add bash script to run everything from scratch.

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
-wget http://www.cs.cmu.edu/~enron/enron_mail_20110402.tgz
-tar xvfz enron_mail_20110402.tgz
-find . -type d  | grep -i sent > sent_folders
-R -f showcase.R
+#!/bin/bash
+
+## download and untar the Enron email corpus
+#wget http://www.cs.cmu.edu/~enron/enron_mail_20110402.tgz
+#tar xvfz enron_mail_20110402.tgz
+
+## create a list of 'sent' folders
+echo "Creating list of 'sent' folders. This may take a moment."
+find . -type d  | grep -i sent  | grep -v presentation > sent_folders
+
+## create an R script with the right working directory (the current dir)
+echo -n 'setwd("' > run.showcase.R
+pwd | tr -d '\r\n'>> run.showcase.R
+echo '")' >> run.showcase.R
+cat showcase.R | grep -v setwd >> run.showcase.R
+
+## run!
+echo "Running R script."
+R -f run.showcase.R
--- showcase.R
+++ showcase.R
@@ -1,3 +1,4 @@
+setwd("/Users/davidr/enron/")
 # Requires library "plyr"
 # Requires 'arcdiagram' which needs to be downloaded from github:
 #   library(devtools)
@@ -6,8 +7,6 @@
 library(plyr)
 library(arcdiagram)
 require(igraph)
-
-setwd("/Users/davidr/enron/")
 
 # Function to read a single email message's headers
 # Adapted from 'Machine Learning for Hackers' (Conway, White)
@@ -61,16 +60,45 @@ colnames(mails) <- c("From", "To")
 mails.sender <- unique(mails$From)
 mails.connected <- subset(mails, To %in% mails.sender)
 mails.counted <- ddply(mails.connected, .(From, To), summarise, weight = length(To))
+mails.counted$From <- sub("@enron.com", "",mails.counted$From)
+mails.counted$To <- sub("@enron.com", "",mails.counted$To)
 g <- graph.data.frame(mails.counted, directed=FALSE)
-d <- sort(degree(g),decreasing=TRUE)
 gg <- delete.edges(g, which(E(g)$weight < 150))
 gg <- delete.vertices(gg, which(degree(gg) < 1))
 
-pdf("out.pdf")
-arcplot(get.edgelist(gg), lwd.arcs = E(gg)$weight/100, cex.labels = 0.45,pch.nodes = 21, lwd.nodes = 2, line = 0, cex.nodes = degree(gg)*0.5)
+pdf("most.mails.pdf")
+arcplot(get.edgelist(gg), lwd.arcs = E(gg)$weight/100, cex.labels = 0.65,pch.nodes = 21, lwd.nodes = 2, line = 0, cex.nodes = degree(gg)*0.5)
+dev.off()
+
+important.people <- c("louise.kitchen@enron.com", "mike.grigsby@enron.com", "greg.whalley@enron.com", "scott.neal@enron.com", "kenneth.lay@enron.com")
+mails.important <- subset(mails, From %in% important.people | To %in% important.people)
+mails.important.counted <- ddply(mails.important, .(From, To), summarise, weight = length(To))
+mails.important.counted$From <- sub("@enron.com", "",mails.important.counted.10$From)
+mails.important.counted$To <- sub("@enron.com", "",mails.important.counted.10$To)
+
+g <- graph.data.frame(mails.important.counted)
+l <- layout.fruchterman.reingold(g,niter=500,area=vcount(g)^2.3,repulserad=vcount(g)^2.8)
+#plot.igraph(g, layout=l, vertex.size=0)
+
+pdf("important.people.pdf", height=34, width=34)
+#arcplot(as.matrix(mails.important.counted.10)[,1:2], lwd.arcs = mails.important.counted.10$weight/10, cex.labels = 0.5,pch.nodes = 20, lwd.nodes = 1, line = -0.2)
+plot.igraph(g, layout=l, vertex.size=0)
 dev.off()
 #gg <- graph.data.frame(subset(mails.connected, From %in% names(top.twenty) && To %in% names(top.twenty)), directed=FALSE)
 
+gg <- delete.vertices(g, which(degree(g) <5))
+ll <- layout.fruchterman.reingold(gg,niter=500,area=vcount(g)^2.3,repulserad=vcount(g)^2.8)
+pdf("important.people.well.connected.pdf", height=34, width=34)
+#arcplot(as.matrix(mails.important.counted.10)[,1:2], lwd.arcs = mails.important.counted.10$weight/10, cex.labels = 0.5,pch.nodes = 20, lwd.nodes = 1, line = -0.2)
+plot.igraph(gg, layout=ll, vertex.size=0)
+dev.off()
+
+gg <- delete.edges(gg, which(E(gg)$weight < 10))
+gg <- delete.vertices(gg, which(degree(gg) < 1))
+
+pdf("important.people.well.connected.weights.pdf")
+arcplot(get.edgelist(gg), lwd.arcs = E(gg)$weight/3,  cex.labels = 0.65,pch.nodes = 21, lwd.nodes = 2, line = 0)
+dev.off()
 # Reduce duplicate entries to one entry, but count the number of occurences
 #mails.counted <- ddply(mails, .(From, To), summarise, count = length(To))
 #mails.counted$From <- sub("@enron.com", "", mails.counted$From)
@@ -86,3 +114,4 @@ dev.off()
 #arcplot(as.matrix(mails.counted)[1:17,1:2])
 #arcplot(as.matrix(mails.connected)[,1:2], lwd.arcs = log(mails.connected$count)+1)
 #arcplot(as.matrix(top.ten[,1:2]), cex.labels = 0.5,lwd.arcs = log(top.ten$count)+1, pch.nodes = 21, lwd.nodes = 2, line = 0, cex.nodes = top.ten$count*0.001)
+
